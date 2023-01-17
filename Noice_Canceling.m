fda3e5f6b8266cecce8f6add30d14afee8b1b48a
ruslan31 <ruslan31bar@gmail.com>
2023-01-17 22:51:47 +0200

    Updates

--- Noice_Canceling.m
+++ Noice_Canceling.m
@@ -1,26 +1,31 @@
 %% Load the signal
 [y,Fs] = audioread('Signal.wav');
 [noise,Fs] = audioread('Noise.wav');
-
+%[y,Fs] = audioread('Mic2.wav');
+%[noise,Fs] = audioread('Mic1.wav');
 %% Add noise to the signal
-signal = y;
-
-
+%signal = y(1:150000);
+%noise = noise(1:150000);
+signal =y;
 %noise = randn(1000,1);
 filt = dsp.FIRFilter;
-filt.Numerator = fir1(11,0.3);
-%fnoise = filt(noise);
-d = signal + noise;
+filt.Numerator = fir1(31,0.5);
+fnoise = filt(noise);
+d = signal+fnoise;
 
 coeffs = (filt.Numerator).'-0.01; % Set the filter initial conditions.
-mu = 0.01; % Set the step size for algorithm updating.
-%coeffs = ones(1,21);
-lms = dsp.LMSFilter(12,'StepSize',mu,'InitialConditions',coeffs);
+mu = 0.0001; % Set the step size for algorithm updating.
+%coeffs = zeros(1,32);
+lms = dsp.LMSFilter(32,'StepSize',mu,'InitialConditions',coeffs);
 [~,e,w] = lms(noise,d);
-subplot(2,1,1)
+subplot(4,1,1)
 plot(e);
-subplot(2,1,2)
+subplot(4,1,2)
 plot(signal);
+subplot(4,1,3)
+plot([e,signal]);
+subplot(4,1,4)
+plot(noise);
 title('Noise Cancellation by the Sign-Data Algorithm');
 legend('Actual signal','Result of noise cancellation',...
        'Location','NorthEast');
@@ -31,3 +36,6 @@ figure()
 stem(coeffs)
 hold on
 stem(w)
+%%
+sound(e,44100)
+
--- Record_and_save.m
+++ Record_and_save.m
@@ -1,22 +1,36 @@
 %Create an audiorecorder object for CD-quality audio in stereo, and view its properties:
 
-recObj = audiorecorder(44100, 16, 1,3);
-get(recObj)
+recorder2 = audiorecorder(44100, 16, 1,1);
+recorder1 = audiorecorder(44100, 16, 1,3);
+%get(recObj)
 
 %Collect a sample of your speech with a microphone, and plot the signal data:
 
 % Record your voice for 5 seconds.
 %recObj = audiorecorder;
+% disp('Start speaking.')
+% recordblocking(recObj, 5);
+% disp('End of Recording.');
+% 
+% % Play back the recording.
+% play(recObj);
+% 
+% % Store data in double-precision array.
+% myRecording_Noise2 = getaudiodata(recObj);
+% 
+% % Plot the waveform.
+% plot(myRecording_Noise2);
+% audiowrite('Noise.wav',myRecording_Noise2,44100)
 disp('Start speaking.')
-recordblocking(recObj, 5);
-disp('End of Recording.');
-
-% Play back the recording.
-play(recObj);
-
-% Store data in double-precision array.
-myRecording_Noise2 = getaudiodata(recObj);
-
-% Plot the waveform.
-plot(myRecording_Noise2);
-audiowrite('Noise.wav',myRecording_Noise2,44100)
+record(recorder1);
+record(recorder2); 
+pause(5);
+stop(recorder1);
+stop(recorder2);
+%%
+plot(getaudiodata(recorder1))
+plot(getaudiodata(recorder2))
+audiowrite('Mic1.wav',getaudiodata(recorder1),44100)
+audiowrite('Mic2.wav',getaudiodata(recorder2),44100)
+%%
+play(recorder2)
